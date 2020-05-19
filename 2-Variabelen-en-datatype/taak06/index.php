eb6aff0a5ed69d55924b60a4b9106e6274645841
jochem <jochem.gravendeel@student.rocva.nl>
2020-05-19 13:02:20 +0200

    taak 6 af

---
+++ 2-Variabelen-en-datatype/taak06/index.php
@@ -0,0 +1,11 @@
+<?php
+    $string = "Jochem";
+    $integer = 1012;
+    $float = 10.12;
+    $boolean = true;
+    $null = null;
+    echo gettype($string)."<br>";
+    echo gettype($integer)."<br>";
+    echo gettype($float)."<br>";
+    echo gettype($boolean)."<br>";
+    echo gettype($null);
