75eebd48cff880eb16002c03f300efc69c91a136
jochem <jochem.gravendeel@student.rocva.nl>
2020-05-19 13:58:14 +0200

    arithmetics af

---
+++ 4-Arithmetics/taak02/index.php
@@ -0,0 +1,10 @@
+<?php
+    $getal1 = 12;
+    $getal1 += 10;
+    echo $getal1.'<br>';
+    $getal1 -= 8;
+    echo $getal1.'<br>';
+    $getal1 *= 2;
+    echo $getal1.'<br>';
+    $getal1 /= 3;
+    echo $getal1;
---
+++ 4-Arithmetics/taak03/index.php
@@ -0,0 +1,3 @@
+<?php
+    $getal1 = 10;
+    echo ++$getal1."<br>".--$getal1;
