1b1327540b246f2ce97b3aa49885f6441ae926eb
jochem <jochem.gravendeel@student.rocva.nl>
2020-05-19 13:09:35 +0200

    taak 1 af

---
+++ 2-Variabelen-en-datatype/taak11/index.php
@@ -0,0 +1,4 @@
+<?php
+    $test = "Jochem";
+    echo gettype($test)."<br>";
+    echo var_dump($test);
---
+++ 3-Arrays/taak01/index.php
@@ -0,0 +1,3 @@
+<?php
+    $_myMovies_ = ["Spiderman","Batman","The Joker"];
+    echo $_myMovies_[0].' '.$_myMovies_[1].' '.$_myMovies_[2];
